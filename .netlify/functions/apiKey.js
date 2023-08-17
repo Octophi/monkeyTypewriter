b365e0d935f2bdf45323c7ff209cb602c9b110ce
joey-li <joey.li@salesforce.com>
2023-08-17 11:14:26 -0400

    Change commonJS module to ES module

--- .netlify/functions/apiKey.js
+++ .netlify/functions/apiKey.js
@@ -1,9 +1,9 @@
-exports.handler = async (event, context) => {
+export async function handler(event, context) {
     const apiKey = process.env.WORDS_API_KEY;
   
     return {
       statusCode: 200,
       body: JSON.stringify({ apiKey })
     };
-  };
+  }
   
